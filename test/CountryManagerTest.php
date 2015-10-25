9ed50620214a6400256e48acfcc69028b89f1462
jpg-consulting <opensource@jpg-consulting.es>
2015-10-25 15:11:56 +0100

    fix PHPUnit tests

--- test/CountryManagerTest.php
+++ test/CountryManagerTest.php
@@ -3,6 +3,7 @@ namespace IsoCodesTest\Country;
 
 use PHPUnit_Framework_TestCase as TestCase;
 use IsoCodes\Country\CountryManager;
+use IsoCodes\Country\Adapter\Xml;
 
 class CountryManagerTest extends TestCase
 {
@@ -14,5 +15,19 @@ class CountryManagerTest extends TestCase
     public function setup()
     {
         $this->countryManager = new CountryManager();
+    }
+
+    /**
+     * @covers IsoCodes\Country\CountryManager::__construct
+     */
+    public function testConstructorAdapter()
+    {
+        $adapter = new Xml(dirname(__DIR__) . '/data/iso_3166.xml');
+        $this->assertInstanceOf('IsoCodes\Country\Adapter\AdapterInterface', $adapter);
+        
+        $countryManager = new CountryManager($adapter);
+        $this->assertInstanceOf('IsoCodes\Country\CountryManager', $countryManager);
+
+        unset($adapter, $countryManager);
     }
 }
